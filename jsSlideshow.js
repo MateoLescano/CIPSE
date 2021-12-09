b2d98328cf1a8ad94d912eb22d8475765d305c6d
MateoLescano <mateolescano@gmail.com>
2021-12-09 15:48:04 -0300

    Contact Style

--- jsSlideshow.js
+++ jsSlideshow.js
@@ -26,15 +26,3 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
-
-// function showSlides() {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {slideIndex = 1}
-//   slides[slideIndex-1].style.display = "block";
-//   setTimeout(showSlides, 5000); // Change image every 2 seconds
-// }
