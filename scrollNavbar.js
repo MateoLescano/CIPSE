cb61ff6ace43bc35dbc2090f079124fcb0a8d1b1
MateoLescano <mateolescano@gmail.com>
2022-01-06 17:12:28 -0300

    margin fix

--- scrollNavbar.js
+++ scrollNavbar.js
@@ -2,9 +2,9 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
+    document.getElementById("navB").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-80px";
+    document.getElementById("navB").style.top = "-110px";
   }
   prevScrollpos = currentScrollPos;
 }
