6151bb756bf83f07a07550b3954dc52307c0f82f
MateoLescano <mateolescano@gmail.com>
2022-01-18 15:18:27 -0300

    select options por fecha

--- optionsSelectDate.js
+++ optionsSelectDate.js
@@ -2,7 +2,7 @@
 var c = document.getElementById("SelectCurso");
 var d = document.getElementById("FechaCurso");
 var arrayC1 = [];
-var arrayC2 = ["25, 26, 27 de Febrero y 4, 5, 6 de Marzo de 2022","6, 7, 8 y 13, 14, 15 de Mayo de 2022","2, 3, 4, y 9, 10, 11 de Septiembre de 2022","2, 3, 4 y 9, 10, 11 de Diciembre de 2022."];
+var arrayC2 = [];
 
 crearArrayFechas();
 
@@ -54,12 +54,18 @@ function clearSelects(id){
 
 function crearArrayFechas(){
     const hoy = new Date();
-    const d1 = new Date("4/3/2022");
-    const d2 = new Date("22/4/2022");
-    const d3 = new Date("3/6/2022");
-    const d4 = new Date("29/7/2022");
-    const d5 = new Date("28/10/2022");
+    const d1 = new Date("3/4/2022");
+    const d2 = new Date("4/22/2022");
+    const d3 = new Date("6/3/2022");
+    const d4 = new Date("7/29/2022");
+    const d5 = new Date("10/28/2022");
 
+    const e1 = new Date("2/25/2022");
+    const e2 = new Date("5/6/2022");
+    const e3 = new Date("9/2/2022");
+    const e4 = new Date("12/2/2022");
+
+    //EMDR PARTE 1
     if (hoy < d1){
         arrayC1.push("4, 5, 6 y 11, 12, 13 de Marzo");
     }
@@ -76,7 +82,17 @@ function crearArrayFechas(){
         arrayC1.push("28, 29, 30 de Octubre y 4, 5, 6 de Noviembre");
     }
 
-    
-    // arrayC1.push("4, 5, 6 y 11, 12, 13 de Marzo de 2022.","22, 23, 24 y 29, 30 de Abril y 1 de Mayo de 2022","3, 4, 5 y 10,11,12 de Junio de 2022","29, 30, 31 de Julio y 5, 6, 7 de Agosto de 2022","28, 29, 30 de Octubre y 4, 5, 6 de Noviembre de 2022.");
-    
+    //EMDR PARTE 2
+    if (hoy < e1){
+        arrayC2.push("25, 26, 27 de Febrero y 4, 5, 6 de Marzo");
+    }
+    if (hoy < e2){
+        arrayC2.push("6, 7, 8 y 13, 14, 15 de Mayo");
+    }
+    if (hoy < e3){
+        arrayC2.push("2, 3, 4, y 9, 10, 11 de Septiembre");
+    }
+    if (hoy < e4){
+        arrayC2.push("2, 3, 4 y 9, 10, 11 de Diciembre");
+    }
 }
